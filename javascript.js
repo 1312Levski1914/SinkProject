12d497e1a3958bc87dccb972205988e9889a8ebc
Levski <krushkov1914@gmail.com>
2021-11-29 01:21:58 +0100

    Home Page
    Mobile and Web home page Done! need to fix nav bar for web!

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+/* Here is code for burger button */
+function openBurgerMenu() {
+    document.getElementById('burger-button').classList.toggle('change');
+    document.getElementById("myDropDown").classList.toggle("show");
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropBurger')) {
+        let dropdowns = document.getElementsByClassName("dropDownContent");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+function showMore(){
+    document.getElementById('moreNav').classList.toggle('show');
+}
+// JS for sound
+function playAudio(url) {
+    new Audio(url).play();
+}
